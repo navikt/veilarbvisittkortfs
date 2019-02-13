f503e31a87a62e4b6ba1cd3a065ac058d9dec66d
alexandraback <alexandra1.back@gmail.com>
2019-02-13 16:03:14 +0100

    KAIZEN-0: Eksport riktigt

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV === 'development') {
     require('./mock');
 }
 
-NAVSPA.eksporter('veilarbvisittkort', App);
+NAVSPA.eksporter('veilarbvisittkortfs', App);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
