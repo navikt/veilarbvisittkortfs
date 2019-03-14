427fe58ff1916a72b7b531064b6e7263af326ed8
alexandraback <alexandra1.back@gmail.com>
2019-03-14 11:46:13 +0100

    KAIZEN-0: Legg til tid/dato på historikkinnslag

--- src/mock/instillingshistorikk.ts
+++ src/mock/instillingshistorikk.ts
@@ -3,14 +3,14 @@ import { JSONValue } from 'yet-another-fetch-mock';
 
 const innstillingsHistorikk: InnstillingsHistorikk[] & JSONValue = [{
     'type': 'SATT_TIL_DIGITAL',
-    'dato': '2019-03-07T07:33:23.29+01:00',
+    'dato': '2019-03-14T11:44:23.29+01:00',
     'begrunnelse': 'ggfdgdf',
     'opprettetAv': 'NAV',
     'opprettetAvBrukerId': 'Z007',
     'dialogId': null
 }, {
     'type': 'SATT_TIL_MANUELL',
-    'dato': '2019-03-07T07:33:15.897+01:00',
+    'dato': '2019-03-14T10:42:15.897+01:00',
     'begrunnelse': 'test',
     'opprettetAv': 'NAV',
     'opprettetAvBrukerId': 'Z007',
