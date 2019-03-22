e79c637fea03aec57847d7014f147fb90005c568
alexandraback <alexandra1.back@gmail.com>
2019-03-22 12:29:41 +0100

    KAIZEN-0:Validering og styling

--- src/app/veilederverktoy/veilederverktoy/begrunnelseform/begrunnelse-textarea.tsx
+++ src/app/veilederverktoy/veilederverktoy/begrunnelseform/begrunnelse-textarea.tsx
@@ -1,5 +1,6 @@
 import FormikTekstArea from '../../../components/formik/formik-textarea';
 import React from 'react';
+import {validerBeskrivelse, validerBeskrivelse500TegnFeldt} from "../../../utils/formik-validation";
 
 export function BegrunnelseTextArea(props: {tekstariaLabel: string, maxLength?: number}) {
     return (
@@ -7,6 +8,7 @@ export function BegrunnelseTextArea(props: {tekstariaLabel: string, maxLength?: 
             labelId={props.tekstariaLabel}
             name="begrunnelse"
             maxLength={props.maxLength || 500}
+            validate={props.maxLength? validerBeskrivelse(props.maxLength) : validerBeskrivelse500TegnFeldt}
         />
     );
 }
