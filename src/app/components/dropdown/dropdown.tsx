2720749d80d1a951aeecb4625961f12688578c4d
alexandraback <alexandra1.back@gmail.com>
2019-11-25 17:22:59 +0100

    KAIZEN-0: Legg tilbake fjern tildelveiledertoast når man tildelar veileder

--- src/app/components/dropdown/dropdown.tsx
+++ src/app/components/dropdown/dropdown.tsx
@@ -3,96 +3,96 @@ import classNames from 'classnames';
 import './dropdown.less';
 import withClickMetric from '../click-metric/click-metric';
 import hiddenIf from '../hidden-if/hidden-if';
-import {useDocumentEventListner} from "../../../hooks/use-event-listner";
+import { useDocumentEventListner } from '../../../hooks/use-event-listner';
 
 /* tslint:disable */
-const btnCls = (erApen: boolean, className: string|undefined) =>
-    classNames('dropdown', className, {
-        'dropdown--apen': erApen,
-    });
+const btnCls = (erApen: boolean, className: string | undefined) =>
+  classNames('dropdown', className, {
+    'dropdown--apen': erApen
+  });
 
 interface DropdownProps {
-    apen?: boolean;
-    name: string;
-    knappeTekst: string;
-    render: (lukkDropdown:()=>void ) => React.ReactNode;
-    className?: string;
-    onLukk?: () => void;
-    onClick?: () => void;
-    btnClassnames?: string;
+  apen?: boolean;
+  name: string;
+  knappeTekst: string;
+  render: (lukkDropdown: () => void) => React.ReactNode;
+  className?: string;
+  onLukk?: () => void;
+  onClick?: () => void;
+  btnClassnames?: string;
 }
 
 function harTrykktPaEsc(e: React.KeyboardEvent) {
-    return e.keyCode === 27;
+  return e.keyCode === 27;
 }
 
 function Dropdown(props: DropdownProps) {
-    const [apen, setApen] = useState(props.apen || false);
-    const btnRef = useRef<HTMLButtonElement>(null);
-    const loggNode = useRef<HTMLDivElement>(null);
-    const { onLukk } = props;
+  const [apen, setApen] = useState(props.apen || false);
+  const btnRef = useRef<HTMLButtonElement>(null);
+  const loggNode = useRef<HTMLDivElement>(null);
+  const { onLukk } = props;
 
-    const lukkDropdown = ()=> {
-        if(apen) {
-            setApen(false);
-            btnRef.current && btnRef.current.focus();
-            onLukk && onLukk();
-        }
-    };
-
-    const eventHandler = (e: any) => {
-        if (loggNode.current && !loggNode.current.contains(e.target)) {
-            lukkDropdown();
-        }
-    };
-
-    useDocumentEventListner('click', eventHandler);
-
-    function apneDropdown() {
-        setApen(true);
+  const lukkDropdown = () => {
+    if (apen) {
+      setApen(false);
+      btnRef.current && btnRef.current.focus();
+      onLukk && onLukk();
     }
+  };
 
-    function toggleDropdown() {
-        if (apen) {
-            lukkDropdown();
-        } else {
-            props.onClick && props.onClick();
-            apneDropdown()
-        }
+  const eventHandler = (e: any) => {
+    if (loggNode.current && !loggNode.current.contains(e.target)) {
+      lukkDropdown();
     }
+  };
 
-    const { name, className, knappeTekst } = props;
-    return (
-        <div className="dropdown" onKeyDown={e => {
-            if(harTrykktPaEsc(e)) {
+  useDocumentEventListner('click', eventHandler);
+
+  function apneDropdown() {
+    setApen(true);
+  }
+
+  function toggleDropdown() {
+    if (apen) {
+      lukkDropdown();
+    } else {
+      props.onClick && props.onClick();
+      apneDropdown();
+    }
+  }
+
+  const { name, className, knappeTekst } = props;
+  return (
+    <div className="dropdown">
+      <div className={btnCls(apen, className)} ref={loggNode}>
+        <button
+          ref={btnRef}
+          type="button"
+          className={classNames('dropdown__btn', props.btnClassnames)}
+          onClick={toggleDropdown}
+          aria-expanded={apen}
+          aria-controls={`${name}-dropdown__innhold`}
+        >
+          {knappeTekst}
+        </button>
+        {apen && (
+          <div
+            className={`${name}-dropdown__innhold dropdown__innhold`}
+            id={`${name}-dropdown__innhold`}
+            onKeyDown={e => {
+              if (harTrykktPaEsc(e)) {
                 e.stopPropagation();
                 e.preventDefault();
                 lukkDropdown();
-            }
-        }}>
-            <div className={btnCls(apen, className)} ref={loggNode}>
-                <button
-                    ref={btnRef}
-                    type="button"
-                    className={classNames("dropdown__btn", props.btnClassnames)}
-                    onClick={toggleDropdown}
-                    aria-expanded={apen}
-                    aria-controls={`${name}-dropdown__innhold`}
-                >
-                    {knappeTekst}
-                </button>
-                {apen &&
-                <div
-                    className={`${name}-dropdown__innhold dropdown__innhold`}
-                    id={`${name}-dropdown__innhold`}
-                >
-
-                    {props.render(lukkDropdown)}
-                </div>
-                }
-            </div>
-        </div>
-    );
+              }
+            }}
+          >
+            {props.render(lukkDropdown)}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default withClickMetric(hiddenIf(Dropdown));
--- src/app/components/radiofilterform/radio-filter-form.tsx
+++ src/app/components/radiofilterform/radio-filter-form.tsx
@@ -6,63 +6,48 @@ import './radio-filterform.less';
 /* tslint:disable */
 
 export interface RadioFilterFormProps<T> {
-    data: T[];
-    createLabel: (foo: T) => string;
-    createValue: (foo: T) => string;
-    radioName: string;
-    closeDropdown: () => void;
-    fjernNullstill?: boolean;
-    visLukkKnapp?: boolean;
-    selected: string;
-    changeSelected: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  data: T[];
+  createLabel: (foo: T) => string;
+  createValue: (foo: T) => string;
+  radioName: string;
+  closeDropdown: () => void;
+  fjernNullstill?: boolean;
+  visLukkKnapp?: boolean;
+  selected: string;
+  changeSelected: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-function RadioFilterForm<T> (props: RadioFilterFormProps<T>) {
-    const {
-        data,
-        createLabel,
-        createValue,
-        radioName,
-        visLukkKnapp,
-    } = props;
+function RadioFilterForm<T>(props: RadioFilterFormProps<T>) {
+  const { data, createLabel, createValue, radioName, visLukkKnapp } = props;
 
-    return (
-        <div className="radio-filterform">
-            <div className="radio-filterform__valg scrollbar">
-                {data.map(o => {
-                    const value = createValue(o);
-                        return (
-                            <Radio
-                                name={radioName}
-                                label={createLabel(o)}
-                                value={value}
-                                id={`${value}-${radioName}`}
-                                key={`${value}-${radioName}`}
-                                checked={value === props.selected}
-                                onChange={e => props.changeSelected(e)}
-                            />
-                        );
-                    }
-                )}
-            </div>
-            <div className="knapperad">
-                <Hovedknapp
-                    hidden={visLukkKnapp && !props.selected}
-                    htmlType="submit"
-                    disabled={!props.selected}
-                    onClick={props.closeDropdown}
-                >
-                    Velg
-                </Hovedknapp>
-                <Knapp
-                    htmlType="button"
-                    onClick={props.closeDropdown}
-                >
-                    Lukk
-                </Knapp>
-            </div>
-        </div>
-    );
+  return (
+    <div className="radio-filterform">
+      <div className="radio-filterform__valg scrollbar">
+        {data.map(o => {
+          const value = createValue(o);
+          return (
+            <Radio
+              name={radioName}
+              label={createLabel(o)}
+              value={value}
+              id={`${value}-${radioName}`}
+              key={`${value}-${radioName}`}
+              checked={value === props.selected}
+              onChange={e => props.changeSelected(e)}
+            />
+          );
+        })}
+      </div>
+      <div className="knapperad">
+        <Hovedknapp hidden={visLukkKnapp && !props.selected} htmlType="submit" disabled={!props.selected}>
+          Velg
+        </Hovedknapp>
+        <Knapp htmlType="button" onClick={props.closeDropdown}>
+          Lukk
+        </Knapp>
+      </div>
+    </div>
+  );
 }
 
 export default RadioFilterForm;
