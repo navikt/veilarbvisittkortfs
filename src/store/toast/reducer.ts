849744773e2e34dc634fc63c65069c906cb94bd3
alexandraback <alexandra1.back@gmail.com>
2019-11-25 16:38:33 +0100

    KAIZEN-0: Legg tilbake fjern tildelveiledertoast når man tildelar veileder

--- src/store/toast/reducer.ts
+++ src/store/toast/reducer.ts
@@ -1,26 +1,27 @@
 import { ToastAction, ToastActionType } from './actions';
 
 export interface ToastState {
-    toasts: ToastActionType[];
+  toasts: ToastActionType[];
 }
 
 const initialState: ToastState = {
-    toasts: []
+  toasts: []
 };
 
 export const toastReducer = (state: ToastState = initialState, action: ToastAction): ToastState => {
-    switch (action.type) {
-        case ToastActionType.VIS_TILDELT_VEILEDER_TOAST:
-            return {
-                toasts: [...state.toasts, action.type]
-            };
-        case ToastActionType.FJERN_TILDET_VEILEDER_TOAST:
-            return {
-                toasts: state.toasts.filter(toast => toast !== ToastActionType.VIS_TILDELT_VEILEDER_TOAST)
-            };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case ToastActionType.VIS_TILDELT_VEILEDER_TOAST:
+      return {
+        toasts: [...state.toasts, action.type]
+      };
+    case ToastActionType.FJERN_TILDET_VEILEDER_TOAST:
+    case ToastActionType.TILDEL_VEILEDER:
+      return {
+        toasts: state.toasts.filter(toast => toast !== ToastActionType.VIS_TILDELT_VEILEDER_TOAST)
+      };
+    default:
+      return state;
+  }
 };
 
 export default toastReducer;
