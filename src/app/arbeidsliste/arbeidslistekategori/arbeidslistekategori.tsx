f5fe9ec2a2b0532a244df9373828833ad41c5188
Tania Holst <tania.holst@nav.no>
2020-03-20 13:08:20 +0100

    KAIZEN-0 fjerna ubrukt kode, linta resten

--- src/app/arbeidsliste/arbeidslistekategori/arbeidslistekategori.tsx
+++ src/app/arbeidsliste/arbeidslistekategori/arbeidslistekategori.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import '../arbeidsliste.less';
 import { ReactComponentElement } from 'react';
+import '../arbeidsliste.less';
 
 interface ArbeidslistekategoriProps {
     value: string;
