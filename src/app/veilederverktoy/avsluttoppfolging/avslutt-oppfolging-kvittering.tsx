deb12fbc207e18ca936209b0c489f586a59ddad5
alexandraback <alexandra1.back@gmail.com>
2020-05-13 14:25:06 +0200

    KAIZEN-0: Bruk window reload istedet for dispatche ett event

--- src/app/veilederverktoy/avsluttoppfolging/avslutt-oppfolging-kvittering.tsx
+++ src/app/veilederverktoy/avsluttoppfolging/avslutt-oppfolging-kvittering.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Kvittering from '../prosess/kvittering';
-import { dispatchOppfolgingAvslutet } from '../../utils/utils';
 
 function AvsluttOppfolgingKvittering() {
     return (
         <Kvittering
             tittel="Avslutt oppfølging fra NAV"
             alertStripeTekst="Oppfølgingsperioden er nå avsluttet."
-            onRequestClose={dispatchOppfolgingAvslutet}
+            onRequestClose={window.location.reload}
         />
     );
 }
