346f42596d6dba0411ed9fcf88f8ab421548b19c
Alexander Gård <alex.gaard@hotmail.com>
2020-12-15 09:35:20 +0100

    Fjernet test workflow

--- src/mock/api/index.ts
+++ src/mock/api/index.ts
@@ -14,7 +14,6 @@ export const allHandlers: RequestHandlersList = [
     ...veilarbaktivitetHandlers,
     ...veilarbdialogHandlers,
     ...veilarboppfolgingHandlers,
-    ...veilarboppfolgingHandlers,
     ...veilarboppgaveHandlers,
     ...veilarbpersonHandlers,
     ...veilarbpersonflatefsHandlers,
