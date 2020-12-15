d8d485fd8a131b1eb4417000d00914285f1bc7c2
AlexGaard <AlexGaard@users.noreply.github.com>
2020-12-15 08:27:53 +0000

    deploy: 2aea5f0135bcfc9e481d79053073dea96f179471

---
+++ precache-manifest.d15f94f2172350688061964a9f91f593.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93fc6d67c97498fcddd19ca88a8d9d67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19971c04b27378124634",
+    "url": "/static/css/main.css"
+  },
+  {
+    "revision": "19971c04b27378124634",
+    "url": "/static/js/main.js"
+  },
+  {
+    "revision": "6d576209d41defaf47ccf9f6c4ac5842",
+    "url": "/static/js/main.js.LICENSE.txt"
+  },
+  {
+    "revision": "601cae7e0cd7cf9ec1704c4083edc86c",
+    "url": "/static/media/advarsel-sirkel.601cae7e.svg"
+  },
+  {
+    "revision": "a76bed7203a30976b3115c8c58c6b147",
+    "url": "/static/media/arbeidsliste.a76bed72.svg"
+  },
+  {
+    "revision": "3908b6adf684e14e15b72a11847d0b3f",
+    "url": "/static/media/arbeidslisteikon_bla.3908b6ad.svg"
+  },
+  {
+    "revision": "c414be90819a0e487c30f2a67cc6124e",
+    "url": "/static/media/arbeidslisteikon_gronn.c414be90.svg"
+  },
+  {
+    "revision": "d6c0fc6b427eff89acb5b946a29f8b23",
+    "url": "/static/media/arbeidslisteikon_gul.d6c0fc6b.svg"
+  },
+  {
+    "revision": "6263f2107dcb55715ee929ad9cfabe5c",
+    "url": "/static/media/arbeidslisteikon_lilla.6263f210.svg"
+  },
+  {
+    "revision": "6127776316df4662d02cf63a706270b6",
+    "url": "/static/media/feil-sirkel.61277763.svg"
+  },
+  {
+    "revision": "a96f3104c87b38c1ff2535939ca895c6",
+    "url": "/static/media/kvinne.a96f3104.svg"
+  },
+  {
+    "revision": "324d51c6b056bfd0586f8bac5a042e49",
+    "url": "/static/media/mann.324d51c6.svg"
+  },
+  {
+    "revision": "97bb60e639366913ee2d1338ae8e3199",
+    "url": "/static/media/ok-sirkel.97bb60e6.svg"
+  },
+  {
+    "revision": "0fdceb0df29e7d72328c19d6b2d72d41",
+    "url": "/static/media/slett.0fdceb0d.svg"
+  },
+  {
+    "revision": "725b3131ecf3df129a7572e57a13d1a8",
+    "url": "/static/media/tannhjul.725b3131.svg"
+  }
+]);
