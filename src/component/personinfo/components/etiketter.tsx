125cf83454f1074bde3bd9655bd0f68431e6c9eb
stianStensli <stian.horn.stensli@nav.no>
2021-12-03 11:01:19 +0100

    Ny manuell etikett logikk

--- src/component/personinfo/components/etiketter.tsx
+++ src/component/personinfo/components/etiketter.tsx
@@ -72,7 +72,7 @@ function Etiketter() {
             </Show>
             <Fokus visible={oppfolging?.underKvp}>KVP</Fokus>
             <Fokus
-                visible={oppfolging && !oppfolging.reservasjonKRR && oppfolging.manuell}
+                visible={oppfolging && oppfolging.manuell}
                 title="Brukeren er vurdert til å ikke kunne benytte seg av aktivitetsplanen og dialogen. Du kan endre til digital oppfølging i Veilederverktøy."
             >
                 Manuell oppfølging
