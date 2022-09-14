30d679e2f71211b2367be44f2db2b38a584b18b1
Klara M Helgemo <klara.margrethe.helgemo@nav.no>
2022-09-14 11:05:54 +0200

    Tc-243 endre kopiknapp fødselsnummer

--- src/component/components/kopier-knapp/kopier-knapp.tsx
+++ src/component/components/kopier-knapp/kopier-knapp.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
 import './kopier-knapp.less';
-import { useState } from 'react';
-import { useEffect } from 'react';
 
 import { CopyToClipboard } from '@navikt/ds-react-internal';
 import { BodyShort } from '@navikt/ds-react';
 
 export function KopierKnappTekst(props: { kopierTekst: string }) {
-    const [copySuccess, setCopySuccess] = useState(false);
-
-    useEffect(() => {
-        let timeOutId = 0;
-        if (copySuccess) {
-            timeOutId = window.setTimeout(() => setCopySuccess(false), 1000);
-        }
-        return () => clearTimeout(timeOutId);
-    }, [copySuccess]);
-
     return (
         <BodyShort as="div" size="small" className="kopier-knapp">
             {props.kopierTekst && (
