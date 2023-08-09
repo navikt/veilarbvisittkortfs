73b158922a5d23aa5b45f9effdcf156966b89358
Klara M Helgemo <klara.margrethe.helgemo@nav.no>
2023-08-09 12:22:52 +0200

    Bruk Aksel-ikoner for kjønn

--- src/component/personinfo/components/kjonn-ikon.tsx
+++ src/component/personinfo/components/kjonn-ikon.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import KvinneIkon from './kvinne.svg';
-import MannIkon from './mann.svg';
+import {
+    FigureInwardFillIcon, FigureOutwardFillIcon
+} from '@navikt/aksel-icons';
 import visibleIf from '../../components/visible-if';
 
 function KjonnIkon(props: { kjonn: string }) {
     const kjonnLowerCase = props.kjonn.toLowerCase();
     // kan ta bort 'k' og 'm' når vi kun henter personalia fra PDL
     if (kjonnLowerCase === 'k' || kjonnLowerCase === 'kvinne') {
-        return <img src={KvinneIkon} className="personinfo__ikon" alt={'kvinne'} />;
+        return <FigureOutwardFillIcon title="kvinne" className="personinfo__ikon" color="darkred" alt={'kvinne'}/>
+
     } else if (kjonnLowerCase === 'm' || kjonnLowerCase === 'mann') {
-        return <img src={MannIkon} className="personinfo__ikon" alt={'mann'} />;
+        return <FigureInwardFillIcon title="mann" className="personinfo__ikon" color="darkblue" alt={'mann'} />;
     }
     return null;
 }
