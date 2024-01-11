dd0ddce7e80e0940b79d6118efda62f479eae5d1
Klara M Helgemo <klara.margrethe.helgemo@nav.no>
2024-01-11 15:56:41 +0100

    class i svg

--- src/component/personinfo/components/kjonn-ikon.tsx
+++ src/component/personinfo/components/kjonn-ikon.tsx
@@ -7,9 +7,9 @@ function KjonnIkon(props: { kjonn: string }) {
     const kjonnLowerCase = props.kjonn.toLowerCase();
     // kan ta bort 'k' og 'm' når vi kun henter personalia fra PDL
     if (kjonnLowerCase === 'k' || kjonnLowerCase === 'kvinne') {
-        return <KvinneIkon className="personinfo__ikon" alt={'kvinne'} />;
+        return <KvinneIkon />;
     } else if (kjonnLowerCase === 'm' || kjonnLowerCase === 'mann') {
-        return <MannIkon className="personinfo__ikon" alt={'mann'} />;
+        return <MannIkon />;
     }
     return null;
 }
