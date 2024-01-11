40a7eb38f1a1c217222a3898ad2ab49efaa1947d
Klara M Helgemo <klara.margrethe.helgemo@nav.no>
2024-01-11 13:23:20 +0100

    msw starter opp i denne versjonen

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { isDevelopment } from './util/utils';
 import * as dayjs from 'dayjs';
 import 'dayjs/locale/nb';
 import App from './app';
-import {createRoot} from "react-dom/client";
+import { createRoot } from 'react-dom/client';
 import { initAmplitude } from './amplitude/amplitude';
 
 dayjs.locale('nb');
@@ -12,13 +12,11 @@ dayjs.locale('nb');
 Navspa.eksporter('veilarbvisittkortfs', App);
 
 if (isDevelopment()) {
-    await import('./mock').then(() => {
-        const rootElement = document.getElementById("veilarbvisittkortfs-root")
-        const root = createRoot(rootElement!)
-        root.render(
-            <App fnr={'10108000398'} enhet={'1234'} tilbakeTilFlate={''} visVeilederVerktoy={true} />
-        );
-    });
+    const { worker } = await import('./mock');
+    await worker.start();
+    const rootElement = document.getElementById('veilarbvisittkortfs-root');
+    const root = createRoot(rootElement!);
+    root.render(<App fnr={'10108000398'} enhet={'1234'} tilbakeTilFlate={''} visVeilederVerktoy={true} />);
 } else {
     initAmplitude();
 }
