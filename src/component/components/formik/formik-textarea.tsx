7687010a23988678ffc6fe437ab16888a54c78ef
Ingrid Fosså <fossa.ingrid@gmail.com>
2024-01-12 12:51:47 +0100

    Unngår å få resize-feil frå designsystemet ved å ikkje rendre text-area før forelderen har fått ei breidd
    Co-authored-by: Julie Hill Roa <julie.hill.roa@nav.no>
    Co-authored-by: Klara Margrethe Helgemo <klara.margrethe.helgemo@nav.no>

--- src/component/components/formik/formik-textarea.tsx
+++ src/component/components/formik/formik-textarea.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Textarea, TextareaProps } from '@navikt/ds-react';
 import { Field, FieldProps } from 'formik';
 import { getErrors } from './formik-utils';
@@ -19,6 +19,17 @@ function FormikTekstArea({
     size = 'medium',
     ...textAreaProps
 }: TekstAreaProps & Omit<TextareaProps, OmitProps>) {
+    const [skjulTextAreaMensKomponentenLaster, setSkjulTextAreaMensKomponentenLaster] = useState(true)
+
+    // Ting vi gjer for å unngå resize-feil frå designsystemet
+    useEffect(() => {
+        setSkjulTextAreaMensKomponentenLaster(false);
+    }, []);
+
+    if (skjulTextAreaMensKomponentenLaster) {
+        return null;
+    }
+
     return (
         <Field validate={validate} name={name}>
             {({ field, form }: FieldProps) => {
