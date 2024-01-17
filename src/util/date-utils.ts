70fcaaf85cb5c99773ddbc8939bf75fc27df395f
JulieHillRoa <julie.hill.roa@nav.no>
2024-01-17 15:10:05 +0100

    Fikse at fjerning av frist i arbeidsliste ikke blir satt til dagens dato men faktisk settes til null

--- src/util/date-utils.ts
+++ src/util/date-utils.ts
@@ -4,15 +4,33 @@ function safeFormat(date: dayjs.Dayjs, format: string): string | undefined {
     return date.isValid() ? date.format(format) : undefined;
 }
 
+/**
+ *
+ * @param dato
+ *
+ * Å sende inn undefined som input her vil gi dagens dato
+ */
 export const dateToISODate = (datoStr: string): string | undefined => {
     const parsetDato = dayjs(datoStr);
     return datoStr && parsetDato.isValid() ? parsetDato.toISOString() : undefined;
 };
 
+/**
+ *
+ * @param dato
+ *
+ * Å sende inn undefined som input her vil gi dagens dato
+ */
 export function formaterDatoKortManed(dato: string | Date | undefined) {
     return safeFormat(dayjs(dato), 'D MMM YYYY');
 }
 
+/**
+ *
+ * @param dato
+ *
+ * Å sende inn undefined som input her vil gi dagens dato
+ */
 export function toSimpleDateStr(dato: string | Date | undefined): string | undefined {
     return safeFormat(dayjs(dato), 'DD.MM.YYYY');
 }
@@ -21,6 +39,12 @@ export function todayReversedDateStr(): string {
     return dayjs().format('YYYY-MM-DD');
 }
 
+/**
+ *
+ * @param dato
+ *
+ * Å sende inn undefined som input her vil gi dagens dato
+ */
 export function toReversedDateStr(dato: string | Date | undefined): string | undefined {
     return safeFormat(dayjs(dato || undefined), 'YYYY-MM-DD');
 }
