6d553fefa9e816bc0c3df4273d22a9873ab2c563
Ingrid Fosså <fossa.ingrid@gmail.com>
2024-01-17 16:26:13 +0100

    Ryddar litt
    - tek vekk unaudsynte fractions
    - litt linjeskift her og der
    - gjer minimumsbreidda på veilederverktøy-modal meir responsiv/UU-venleg

--- src/component/veilederverktoy/prosess/kvittering.tsx
+++ src/component/veilederverktoy/prosess/kvittering.tsx
@@ -24,13 +24,11 @@ function Kvittering({ tittel, alertStripeTekst, footer, onRequestClose }: Kvitte
             }}
             type="SUCCESS"
         >
-            <>
-                <Heading size="medium" as="h2" className="modal-info-tekst__undertekst">
-                    {tittel}
-                </Heading>
-                <BodyShort size="small">{alertStripeTekst}</BodyShort>
-                {!!footer && <BodyShort size="medium">{footer}</BodyShort>}
-            </>
+            <Heading size="medium" as="h2" className="modal-info-tekst__undertekst">
+                {tittel}
+            </Heading>
+            <BodyShort size="small">{alertStripeTekst}</BodyShort>
+            {!!footer && <BodyShort size="medium">{footer}</BodyShort>}
         </VarselModal>
     );
 }
