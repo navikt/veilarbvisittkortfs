8b05cfec022862e8954f349d405ad16bf60308aa
Oda Dahlen <oda.dahlen@nav.no>
2024-01-24 15:43:06 +0100

    Fjerner alert ved rediger i kommentarfeltet

--- src/component/huskelapp/redigering/huskelapp-form.tsx
+++ src/component/huskelapp/redigering/huskelapp-form.tsx
@@ -3,27 +3,33 @@ import FormikTekstArea from '../../components/formik/formik-textarea';
 import FormikDatoVelger from '../../components/formik/formik-datepicker';
 import { validerFristFelt, validerHuskelappKommentarFelt } from '../../../util/formik-validation';
 import { Alert } from '@navikt/ds-react';
+import { useDataStore } from '../../../store/data-store';
 
 interface Props {
     visManglerFristOgKommentarHuskelapp: boolean;
 }
 
-export const HuskelappForm = (props: Props) => (
-    <>
-        {props.visManglerFristOgKommentarHuskelapp && (
-            <Alert inline variant="error">
-                Du må legge til enten frist eller kommentar for å kunne lagre huskelappen
-            </Alert>
-        )}
-        <FormikTekstArea
-            name="kommentar"
-            label=""
-            maxLength={100}
-            validate={validerHuskelappKommentarFelt}
-            className="margin-bottom-s"
-        />
-        <FormikDatoVelger name="frist" validate={validerFristFelt} label="Frist" ariaLabel="Frist for huskelapp" />
-    </>
-);
+export function HuskelappForm(props: Props) {
+    const { visFeilHuskelapp, setVisFeilHuskelapp } = useDataStore();
+
+    return (
+        <>
+            {visFeilHuskelapp && (
+                <Alert inline variant="error">
+                    Du må legge til enten frist eller kommentar for å kunne lagre huskelappen
+                </Alert>
+            )}
+            <FormikTekstArea
+                name="kommentar"
+                label=""
+                maxLength={100}
+                validate={validerHuskelappKommentarFelt}
+                className="margin-bottom-s"
+                onChangeCapture={() => setVisFeilHuskelapp(false)}
+            />
+            <FormikDatoVelger name="frist" validate={validerFristFelt} label="Frist" ariaLabel="Frist for huskelapp" />
+        </>
+    );
+}
 
 export default HuskelappForm;
