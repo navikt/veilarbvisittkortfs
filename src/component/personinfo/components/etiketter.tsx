e524409181245b9e9b4f7d77bf47f14abdf44333
Dragutin Vujovic <dragutin.vujovic@nav.no>
2024-01-30 15:00:25 +0100

    Små fiks

--- src/component/personinfo/components/etiketter.tsx
+++ src/component/personinfo/components/etiketter.tsx
@@ -115,7 +115,7 @@ function Etiketter() {
             <Fokus visible={!oppfolging?.underOppfolging}>Ikke under oppfølging</Fokus>
             <Fokus visible={gjeldendeEskaleringsvarsel}>Varsel</Fokus>
             <Fokus
-                visible={!oppfolging?.reservasjonKRR && !oppfolging?.manuell && !oppfolging?.kanVarsles}
+                visible={!oppfolging?.reservasjonKRR}
                 title="Brukeren er ikke registrert i Kontakt- og reservasjonsregisteret og kan ikke varsles. Du kan derfor ikke samhandle digitalt med brukeren. "
             >
                 Ikke registrert KRR
