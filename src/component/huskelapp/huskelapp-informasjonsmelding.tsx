4e16fe7aac68102c89e97e7b2a9cfac8d557ddf1
Mathias S. Müller <mathias.sundt.muller@nav.no>
2024-05-03 16:39:28 +0200

    Oppdater huskelappinfo-alert

--- src/component/huskelapp/huskelapp-informasjonsmelding.tsx
+++ src/component/huskelapp/huskelapp-informasjonsmelding.tsx
@@ -4,7 +4,7 @@ import { trackAmplitude } from '../../amplitude/amplitude';
 
 export const HuskelappInformasjonsmelding = () => (
     <Alert variant="info" className="huskelapp-alert" size="small">
-        Huskelapp er ikke synlig for personen det gjelder...{' '}
+        Bruk huskelappen til informasjon som er viktig for oppfølgingen av personen.{' '}
         <Link
             href="https://navno.sharepoint.com/sites/fag-og-ytelser-arbeid-arbeidsrettet-brukeroppfolging/SitePages/Arbeidslisten-i-Oversikten-i-Modia.aspx"
             target="_blank"
@@ -13,11 +13,11 @@ export const HuskelappInformasjonsmelding = () => (
             onClick={() => {
                 trackAmplitude({
                     name: 'navigere',
-                    data: { lenketekst: 'Les om hvordan bruke huskelapp på Navet', destinasjon: 'navet' }
+                    data: { lenketekst: 'Oppdatert info om bruk av huskelappen', destinasjon: 'navet' }
                 });
             }}
         >
-            Les mer om hvordan bruke huskelapp på Navet <ExternalLinkIcon />
+            Oppdatert info om bruk av huskelappen <ExternalLinkIcon />
         </Link>
     </Alert>
 );
