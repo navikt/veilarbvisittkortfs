e4457c8ae6e7aafe97a7bd47332bd47be2a9e6db
Mathias S. Müller <mathias.sundt.muller@nav.no>
2024-05-03 16:49:27 +0200

    Juster gammelt arbeidslisteinnhold

--- src/component/huskelapp/redigering/gammelArbeidsliste.tsx
+++ src/component/huskelapp/redigering/gammelArbeidsliste.tsx
@@ -1,8 +1,7 @@
-import { BodyLong, BodyShort, Heading, List } from '@navikt/ds-react';
+import { BodyLong, BodyShort, Detail, Heading, List } from '@navikt/ds-react';
 import { Arbeidsliste } from '../../../api/veilarbportefolje';
 import { toSimpleDateStr } from '../../../util/date-utils';
 import './huskelapp-redigering.less';
-import { SlettArbeidsliste } from './huskelapp-slett-arbeidsliste';
 
 interface Props {
     arbeidsliste: Arbeidsliste | undefined;
@@ -10,40 +9,37 @@ interface Props {
 
 export const GammelArbeidsliste = ({ arbeidsliste }: Props) => {
     return (
-        <div>
-            <div className="arbeidsliste-innhold">
-                <Heading level="2" size="small">
-                    Gammel arbeidsliste
+        <div className="arbeidsliste-innhold">
+            <Heading level="2" size="small">
+                Gammel arbeidsliste
+            </Heading>
+            <List size="small">
+                <List.Item>Kopier det du vil ha med fra gammel arbeidsliste, og legg i ny huskelapp.</List.Item>
+                <List.Item>
+                    Gammel arbeidsliste slettes automatisk når du lagrer ny huskelapp, fargekategori beholdes.
+                </List.Item>
+            </List>
+            <div className="arbeidsliste-innhold-tekst">
+                <Heading level="3" size="xsmall">
+                    {arbeidsliste?.overskrift}
                 </Heading>
-                <List size="small">
-                    <List.Item>
-                        Merk og kopiér det du vil ha med fra gammel arbeidsliste, og legg i ny huskelapp.
-                    </List.Item>
-                    <List.Item>
-                        Gammel arbeidsliste slettes automatisk når du lagrer ny huskelapp, fargemerking beholdes.
-                    </List.Item>
-                </List>
-                <BodyLong size="large" className="arbeidsliste-innhold-tekst">
-                    <Heading level="3" size="xsmall">
-                        {arbeidsliste?.overskrift}
-                    </Heading>
-                    <BodyShort size="small">
-                        <i>Arbeidsliste frist: {toSimpleDateStr(arbeidsliste?.frist ?? 'Ingen frist satt')}</i>
-                    </BodyShort>
-                    <BodyLong size="small">{arbeidsliste?.kommentar}</BodyLong>
-                    <BodyShort size="small">
-                        <i>
-                            {`Oppdatert ${
-                                arbeidsliste?.endringstidspunkt
-                                    ? toSimpleDateStr(arbeidsliste.endringstidspunkt)
-                                    : 'ukjent'
-                            }`}
-                            {arbeidsliste?.sistEndretAv?.veilederId && ` av ${arbeidsliste.sistEndretAv.veilederId}`}
-                        </i>
-                    </BodyShort>
+                <BodyLong size="small" spacing>
+                    {arbeidsliste?.kommentar}
                 </BodyLong>
+                <BodyShort size="small" spacing className="arbeidsliste-innhold-frist">
+                    {arbeidsliste?.frist && (
+                        <i>Arbeidslistefrist: {toSimpleDateStr(arbeidsliste?.frist ?? 'Ingen frist satt')}</i>
+                    )}
+                </BodyShort>
             </div>
-            <SlettArbeidsliste />
+            <Detail>
+                <i>
+                    {`Oppdatert ${
+                        arbeidsliste?.endringstidspunkt ? toSimpleDateStr(arbeidsliste.endringstidspunkt) : 'ukjent'
+                    }`}
+                    {arbeidsliste?.sistEndretAv?.veilederId && ` av ${arbeidsliste.sistEndretAv.veilederId}`}
+                </i>
+            </Detail>
         </div>
     );
 };
