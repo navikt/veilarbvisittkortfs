ffd0725aa54c62da414dc14c7cb0ab319c2f5d02
Klara M Helgemo <klara.margrethe.helgemo@nav.no>
2024-05-30 15:21:27 +0200

    Legger til tooltip på fargekategoriknappen (ikke arbeidslisteknappen)

--- src/component/fargekategori/fargekategoriknapp.tsx
+++ src/component/fargekategori/fargekategoriknapp.tsx
@@ -1,8 +1,9 @@
 import React, { useRef, useState } from 'react';
 import { useDataStore } from '../../store/data-store';
-import { Button } from '@navikt/ds-react';
+import { Button, Tooltip } from '@navikt/ds-react';
 import { mapfargekategoriToIkon } from './mapfargekategoriToIkon';
 import { FargekategoriPopover } from './fargekategori-popover';
+import { utvidetFargekategoriModell } from '../../api/veilarbportefolje';
 
 interface Props {
     hidden: boolean;
@@ -13,21 +14,27 @@ export const Fargekategoriknapp = ({ hidden }: Props) => {
     const { fargekategori, setFargekategori } = useDataStore();
     const [isPopoverOpen, setIsPopoverOpen] = useState(false);
 
+    const tooltip = fargekategori?.fargekategoriVerdi
+        ? utvidetFargekategoriModell.find(f => f.key === fargekategori?.fargekategoriVerdi)?.beskrivelse
+        : 'Ingen kategori';
+
     if (hidden) {
         return null;
     }
 
     return (
         <>
-            <Button
-                ref={buttonRef}
-                icon={mapfargekategoriToIkon(fargekategori?.fargekategoriVerdi ?? null)}
-                className="fargekategori-knapp"
-                onClick={() => setIsPopoverOpen(true)}
-                variant="tertiary-neutral"
-                size="medium"
-                aria-expanded={isPopoverOpen}
-            />
+            <Tooltip content={`${tooltip} endre fargekategori`}>
+                <Button
+                    ref={buttonRef}
+                    icon={mapfargekategoriToIkon(fargekategori?.fargekategoriVerdi ?? null)}
+                    className="fargekategori-knapp"
+                    onClick={() => setIsPopoverOpen(true)}
+                    variant="tertiary-neutral"
+                    size="medium"
+                    aria-expanded={isPopoverOpen}
+                />
+            </Tooltip>
             <FargekategoriPopover
                 buttonRef={buttonRef}
                 isOpen={isPopoverOpen}
