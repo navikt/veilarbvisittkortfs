8fbeb99ef1620beb429235e295bbb57e9ba65539
Ingrid Fosså <ingrid.fossa@nav.no>
2024-06-06 14:50:48 +0200

    Ikkje gå til huskelappvisning etter lagring av huskelapp

--- src/component/huskelapp/redigering/huskelapp-redigere-modal.tsx
+++ src/component/huskelapp/redigering/huskelapp-redigere-modal.tsx
@@ -29,7 +29,7 @@ const huskelappEmptyValues = {
 
 function HuskelappRedigereModal() {
     const { brukerFnr, enhetId } = useAppStore();
-    const { hideModal, showSpinnerModal, showErrorModal, showHuskelappModal } = useModalStore();
+    const { hideModal, showSpinnerModal, showErrorModal } = useModalStore();
     const { huskelapp, setHuskelapp, arbeidsliste, setArbeidsliste } = useDataStore();
 
     const erArbeidslistaTom =
@@ -97,7 +97,7 @@ function HuskelappRedigereModal() {
                 .then(() => fetchHuskelapp(brukerFnr.toString(), enhetId ?? ''))
                 .then(res => res.data)
                 .then(setHuskelapp)
-                .then(showHuskelappModal)
+                .then(hideModal)
                 .then(() => {
                     if (!erArbeidslistaTom) {
                         slettArbeidslisteMenIkkeFargekategori(brukerFnr)
@@ -116,7 +116,7 @@ function HuskelappRedigereModal() {
                 .then(() => fetchHuskelapp(brukerFnr.toString(), enhetId ?? ''))
                 .then(res => res.data)
                 .then(setHuskelapp)
-                .then(showHuskelappModal)
+                .then(hideModal)
                 .then(() => {
                     slettArbeidslisteMenIkkeFargekategori(brukerFnr)
                         .then(res => res.data)
