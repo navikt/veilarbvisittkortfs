fa8e2fd7fb3d9cc99ea8c2375432e338cd3715a3
Ingrid Fosså <ingrid.fossa@nav.no>
2024-11-04 15:30:40 +0100

    Fjern arbeidsliste-innhald skjult bak feature-toggle

--- src/component/huskelapp/huskelapp-informasjonsmelding.tsx
+++ src/component/huskelapp/huskelapp-informasjonsmelding.tsx
@@ -1,50 +1,21 @@
 import { Alert, Link } from '@navikt/ds-react';
 import { trackAmplitude } from '../../amplitude/amplitude';
-import { useDataStore } from '../../store/data-store';
-import { SKJUL_ARBEIDSLISTEFUNKSJONALITET } from '../../api/veilarbpersonflatefs';
 
-export const HuskelappInformasjonsmelding = () => {
-    const { features } = useDataStore();
-    const arbeidslistefunksjonalitetSkalVises = !features[SKJUL_ARBEIDSLISTEFUNKSJONALITET];
-
-    if (arbeidslistefunksjonalitetSkalVises) {
-        return (
-            <Alert variant="warning" className="huskelapp-alert" size="small">
-                <Link
-                    href="https://navno.sharepoint.com/sites/fag-og-ytelser-arbeid-arbeidsrettet-brukeroppfolging/SitePages/Arbeidslisten-i-Oversikten-i-Modia.aspx"
-                    target="_blank"
-                    rel="noopener"
-                    inlineText
-                    onClick={() => {
-                        trackAmplitude({
-                            name: 'navigere',
-                            data: { lenketekst: 'Oppdatert info om bruk av huskelappen', destinasjon: 'navet' }
-                        });
-                    }}
-                >
-                    Gamle arbeidslister blir slettet 25. oktober
-                </Link>{' '}
-                (åpnes i ny fane)
-            </Alert>
-        );
-    } else {
-        return (
-            <Alert variant="info" className="huskelapp-alert" size="small">
-                <Link
-                    href="https://navno.sharepoint.com/sites/fag-og-ytelser-arbeid-arbeidsrettet-brukeroppfolging/SitePages/Arbeidslisten-i-Oversikten-i-Modia.aspx"
-                    target="_blank"
-                    rel="noopener"
-                    inlineText
-                    onClick={() => {
-                        trackAmplitude({
-                            name: 'navigere',
-                            data: { lenketekst: 'Oppdatert info om bruk av huskelappen', destinasjon: 'navet' }
-                        });
-                    }}
-                >
-                    Personvern i huskelappen - har du lov, og er det nødvendig? (Navet)
-                </Link>
-            </Alert>
-        );
-    }
-};
+export const HuskelappInformasjonsmelding = () => (
+    <Alert variant="info" className="huskelapp-alert" size="small">
+        <Link
+            href="https://navno.sharepoint.com/sites/fag-og-ytelser-arbeid-arbeidsrettet-brukeroppfolging/SitePages/Arbeidslisten-i-Oversikten-i-Modia.aspx"
+            target="_blank"
+            rel="noopener"
+            inlineText
+            onClick={() => {
+                trackAmplitude({
+                    name: 'navigere',
+                    data: { lenketekst: 'Oppdatert info om bruk av huskelappen', destinasjon: 'navet' }
+                });
+            }}
+        >
+            Personvern i huskelappen - har du lov, og er det nødvendig? (Navet)
+        </Link>
+    </Alert>
+);
--- src/component/huskelapp/huskelapp-knapp.tsx
+++ src/component/huskelapp/huskelapp-knapp.tsx
@@ -3,10 +3,8 @@ import withClickMetric from '../components/click-metric/click-metric';
 import { trackAmplitude } from '../../amplitude/amplitude';
 import HuskelappInaktivIkon from './ikon/Huskelappikon_stiplet.svg?react';
 import HuskelappIkon from './ikon/Huskelappikon_bakgrunnsfarge.svg?react';
-import { useArbeidsliste, useHuskelapp } from '../../api/veilarbportefolje';
+import { useHuskelapp } from '../../api/veilarbportefolje';
 import { feilErIkke400, feilErIkke403 } from './harTilgangTilHuskelapp';
-import { useDataStore } from '../../store/data-store';
-import { SKJUL_ARBEIDSLISTEFUNKSJONALITET } from '../../api/veilarbpersonflatefs';
 
 interface Props {
     brukerFnr: string;
@@ -15,22 +13,15 @@ interface Props {
 }
 
 function HuskelappKnapp({ brukerFnr, visVeilederVerktoy, onClick }: Props) {
-    const { data: arbeidsliste, error: arbeidslisteError } = useArbeidsliste(brukerFnr, visVeilederVerktoy);
-    const { features } = useDataStore();
-    const arbeidslistefunksjonalitetSkalVises = !features[SKJUL_ARBEIDSLISTEFUNKSJONALITET];
-
     const {
         data: huskelapp,
         isLoading: huskelappIsLoading,
         error: huskelappError
     } = useHuskelapp(brukerFnr, visVeilederVerktoy);
 
-    const harArbeidsliste = arbeidsliste?.sistEndretAv != null;
     const harHuskelapp = huskelapp?.huskelappId != null;
-    const harHuskelappEllerArbeidsliste = harHuskelapp || (arbeidslistefunksjonalitetSkalVises && harArbeidsliste);
 
-    const hasError =
-        (feilErIkke403(huskelappError) && feilErIkke400(huskelappError)) || feilErIkke403(arbeidslisteError);
+    const hasError = feilErIkke403(huskelappError) && feilErIkke400(huskelappError);
 
     const handleClick = () => {
         trackAmplitude({
@@ -50,8 +41,8 @@ function HuskelappKnapp({ brukerFnr, visVeilederVerktoy, onClick }: Props) {
             {!hasError && (
                 <Button
                     variant="tertiary"
-                    icon={harHuskelappEllerArbeidsliste ? <HuskelappIkon /> : <HuskelappInaktivIkon />}
-                    title={harHuskelappEllerArbeidsliste ? 'Endre huskelapp' : 'Opprett huskelapp'}
+                    icon={harHuskelapp ? <HuskelappIkon /> : <HuskelappInaktivIkon />}
+                    title={harHuskelapp ? 'Endre huskelapp' : 'Opprett huskelapp'}
                     onClick={handleClick}
                     loading={huskelappIsLoading}
                 />
