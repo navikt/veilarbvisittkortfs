cebf9fb14a908940c316453edf56ddc270315933
Ingrid Fosså <ingrid.fossa@nav.no>
2024-12-17 16:54:02 +0100

    Tek bort formattering av landkode sidan koden berre kjente att norske landkodar (og dei vart fjerna)

--- src/util/formaterTelefonnummer.ts
+++ src/util/formaterTelefonnummer.ts
@@ -3,41 +3,31 @@ import { StringOrNothing } from './type/utility-types';
 const emdashCharacterCode = 8212;
 const EMDASH = String.fromCharCode(emdashCharacterCode);
 
-export function formaterTelefonnummer(telefon: StringOrNothing): string {
-    let norskTelefonnummer = false;
-
-    if (!telefon) {
+export function formaterTelefonnummer(uformatertTelefonnummer: StringOrNothing): string {
+    if (!uformatertTelefonnummer) {
         return EMDASH;
     }
 
-    // Fjernar mellomrom frå telefonnummer
-    let telefonNr = telefon.replace(/\s/g, '');
+    // Fjernar eventuelle mellomrom frå telefonnummer
+    let telefonnummer = uformatertTelefonnummer.replace(/\s/g, '');
 
-    // Sjekk om det er eit norsk telefonnummer
-    let landkode = '';
-    if (telefonNr.startsWith('0047')) {
-        landkode = '+47';
-        telefonNr = telefonNr.slice(4);
-        norskTelefonnummer = true;
-    } else if (telefonNr.startsWith('+47')) {
-        landkode = telefonNr.slice(0, 3);
-        telefonNr = telefonNr.slice(3);
-        norskTelefonnummer = true;
+    // Tek bort landkode frå norske telefonnummer
+    if (telefonnummer.startsWith('0047')) {
+        telefonnummer = telefonnummer.slice(4);
+    } else if (telefonnummer.startsWith('+47')) {
+        telefonnummer = telefonnummer.slice(3);
     }
 
-    const tall = telefonNr.split('');
-    const splittTall = [];
-
-    while (tall.length) {
-        splittTall.push(tall.splice(0, 2).join(''));
-    }
-
-    if (norskTelefonnummer) {
-        return splittTall.join(' ');
-    } else if (landkode.length) {
-        // Dette vil ikkje skje med mindre telefonnummer er norsk eller vi endrar attkjenning av landkodar. Eg let koden liggje fordi den illustrerar korleis vi skulle ynskje det var. 2024-12-17, Ingrid.
-        return `${landkode} ${splittTall.join(' ')}`;
-    } else {
-        return `${splittTall.join(' ')}`;
-    }
+    return lagStrengMedToOgToTegnGruppert(telefonnummer);
 }
+
+const lagStrengMedToOgToTegnGruppert = (telefonnummer: string): string => {
+    const ettOgEttSifferListe: string[] = telefonnummer.split('');
+    const toOgToSifferListe: string[] = [];
+
+    while (ettOgEttSifferListe.length) {
+        toOgToSifferListe.push(ettOgEttSifferListe.splice(0, 2).join(''));
+    }
+
+    return toOgToSifferListe.join(' ');
+};
