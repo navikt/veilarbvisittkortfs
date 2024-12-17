ef39bf9fc25675ca04cda73f50a03e76eff457e2
Ingrid Fosså <ingrid.fossa@nav.no>
2024-12-17 16:52:33 +0100

    Rydd i formaterTelefonnummer
    - La koden stole på sine eigne nullsjekkar
    - Legg til forklaringar på kva som skjer der eg forstår det
    - Innsjå at vi treng testar, for vi får truleg ingen landskode på telefonnummer som ikkje er norske.

--- src/util/utils.ts
+++ src/util/utils.ts
@@ -65,25 +65,29 @@ export function formaterTelefonnummer(telefon: StringOrNothing) {
     if (!telefon) {
         return EMDASH;
     }
-    let telefonNr = telefon?.toString().replace(/\s/g, '');
-    let landkode = '';
 
-    if (telefonNr && telefonNr.startsWith('0047')) {
+    // Fjernar mellomrom frå telefonnummer
+    let telefonNr = telefon.replace(/\s/g, '');
+
+    // Sjekk om det er eit norsk telefonnummer
+    let landkode = '';
+    if (telefonNr.startsWith('0047')) {
         landkode = '+47';
         telefonNr = telefonNr.slice(4);
         norskTelefonnummer = true;
-    } else if (telefonNr && telefonNr.startsWith('+47')) {
+    } else if (telefonNr.startsWith('+47')) {
         landkode = telefonNr.slice(0, 3);
         telefonNr = telefonNr.slice(3);
         norskTelefonnummer = true;
     }
 
-    const tall = telefonNr?.split('');
+    const tall = telefonNr.split('');
     const splittTall = [];
 
-    while (tall?.length) {
+    while (tall.length) {
         splittTall.push(tall.splice(0, 2).join(''));
     }
+
     if (norskTelefonnummer) {
         return splittTall.join(' ');
     } else return `${landkode} ${splittTall.join(' ')}`;
