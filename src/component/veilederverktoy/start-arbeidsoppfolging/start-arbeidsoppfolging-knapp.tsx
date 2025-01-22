0fb69c9fbf808bba79d748f27811c9e96464f8a6
sigurdgroneng <sigurd.groneng@nav.no>
2025-01-22 14:52:58 +0100

    Fix urls

--- src/component/veilederverktoy/start-arbeidsoppfolging/start-arbeidsoppfolging-knapp.tsx
+++ src/component/veilederverktoy/start-arbeidsoppfolging/start-arbeidsoppfolging-knapp.tsx
@@ -4,11 +4,11 @@ import { EnvType, getEnv } from '../../../util/envUtil';
 const env = getEnv();
 const startArbeidsoppfolgingUrl: Record<`${EnvType}-${'ansatt' | 'intern'}`, string> = {
     [`${EnvType.prod}-ansatt`]: '??',
-    [`${EnvType.dev}-ansatt`]: 'start-arbeidsoppfolging.ansatt.dev.nav.no',
-    [`${EnvType.local}-ansatt`]: 'inngar.intern.dev.nav.no',
+    [`${EnvType.dev}-ansatt`]: 'https://start-arbeidsoppfolging.ansatt.dev.nav.no',
+    [`${EnvType.local}-ansatt`]: 'https://start-arbeidsoppfolging.ansatt.dev.nav.no',
     [`${EnvType.prod}-intern`]: '??',
-    [`${EnvType.dev}-intern`]: 'inngar.intern.dev.nav.no',
-    [`${EnvType.local}-intern`]: 'inngar.intern.dev.nav.no'
+    [`${EnvType.dev}-intern`]: 'https://inngar.intern.dev.nav.no',
+    [`${EnvType.local}-intern`]: 'https://inngar.intern.dev.nav.no'
 };
 const url = startArbeidsoppfolgingUrl[`${env.type}-${env.ingressType}`];
 
