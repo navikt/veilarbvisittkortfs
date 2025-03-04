50222e08b7e690714de13dff6e50766fff21070e
Hans Petter Simonsen <hans.petter.simonsen@nav.no>
2025-03-04 13:12:24 +0100

    Alltid vise Arbeidssøkerregisteret i veilederverktøy + ny ansatt-ingress

--- src/component/veilederverktoy/start-arbeidssoker-registrering/start-arbeidssoker-registrering-knapp.tsx
+++ src/component/veilederverktoy/start-arbeidssoker-registrering/start-arbeidssoker-registrering-knapp.tsx
@@ -2,14 +2,13 @@ import { useAppStore } from '../../../store/app-store';
 import { erITestMiljo } from '../../../util/utils';
 import { logMetrikk } from '../../../util/logger';
 import { useDataStore } from '../../../store/data-store';
-import { kanRegistreresEllerReaktiveres } from '../../../util/selectors';
 import { Dropdown } from '@navikt/ds-react';
 import { useOpplysningerOmArbeidssokerMedProfilering } from '../../../api/veilarbperson';
 
 //@todo: check with arbeidssokerregistrering if they can fetch fnr from modiacontext holder
 function byggRegistreringUrl() {
     return erITestMiljo()
-        ? `https://arbeidssokerregistrering-for-veileder.intern.dev.nav.no`
+        ? `https://arbeidssokerregistrering-for-veileder.intern.ansatt.nav.no`
         : `https://arbeidssokerregistrering-for-veileder.intern.nav.no/`;
 }
 
@@ -18,22 +17,15 @@ export const StartArbeidssokerRegistreringKnapp = () => {
     const { oppfolging } = useDataStore();
     const { data: opplysningerOmArbeidssoker } = useOpplysningerOmArbeidssokerMedProfilering(brukerFnr);
 
-    const kanRegistreres = kanRegistreresEllerReaktiveres(oppfolging);
     const registreringUrl = byggRegistreringUrl();
 
-    if (!kanRegistreres && !!opplysningerOmArbeidssoker?.arbeidssoekerperiodeStartet) {
-        return null;
-    }
     const brukerType =
         oppfolging?.kanReaktiveres && !opplysningerOmArbeidssoker?.arbeidssoekerperiodeStartet
             ? 'kanReaktiveres'
             : 'kanIkkeReaktiveres';
 
     const brukerTekst = () => {
-        if (brukerType === 'kanReaktiveres') {
-            return 'Reaktiver arbeidssøker';
-        }
-        return 'Registrer arbeidssøker';
+        return 'Arbeidssøkerregisteret';
     };
 
     return (
