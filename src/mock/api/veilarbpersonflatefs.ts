b398c6364f4f41678d70cdd5fbf21fb3b9126608
Sondre Larsen Ovrid <sondre.larsen.ovrid@nav.no>
2025-04-09 12:51:38 +0200

    Legg til default-verdiar for nye feature-toggles i mocken

--- src/mock/api/veilarbpersonflatefs.ts
+++ src/mock/api/veilarbpersonflatefs.ts
@@ -1,9 +1,18 @@
-import { VIS_NY_INNGANG_TIL_ARBEIDSRETTET_OPPFOLGING, OboUnleashFeatures } from '../../api/veilarbpersonflatefs';
+import {
+    BRUK_GJELDENDE_14A_SOM_KILDE_FOR_PROFILERINGSETIKETTER,
+    BRUK_GJELDENDE_14A_SOM_KILDE_FOR_TRENGER_VURDERING_ETIKETT,
+    VIS_NY_INNGANG_TIL_ARBEIDSRETTET_OPPFOLGING,
+    OboUnleashFeatures
+} from '../../api/veilarbpersonflatefs';
 import { defaultNetworkResponseDelay } from '../config';
 import { delay, http, HttpResponse, RequestHandler } from 'msw';
+
 const mockFeatures: OboUnleashFeatures = {
-    [VIS_NY_INNGANG_TIL_ARBEIDSRETTET_OPPFOLGING]: true
+    [VIS_NY_INNGANG_TIL_ARBEIDSRETTET_OPPFOLGING]: true,
+    [BRUK_GJELDENDE_14A_SOM_KILDE_FOR_PROFILERINGSETIKETTER]: true,
+    [BRUK_GJELDENDE_14A_SOM_KILDE_FOR_TRENGER_VURDERING_ETIKETT]: true
 };
+
 export const veilarbpersonflatefsHandlers: RequestHandler[] = [
     http.get('obo-unleash/api/feature', async () => {
         await delay(defaultNetworkResponseDelay);
