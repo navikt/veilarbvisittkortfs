4c2ffd8e28c2fabbf7e29f8a88e8326263ce0d70
sigurdgroneng <sigurd.groneng@nav.no>
2026-01-09 09:57:54 +0100

    Replace modal-store

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,6 @@ import PersonInfo from './component/personinfo/personinfo';
 import { Veilederverktoy } from './component/veilederverktoy/veilederverktoy';
 import Etiketter from './component/personinfo/components/etiketter';
 import Tilbakelenke from './component/components/tilbakelenke/tilbakelenke';
-import StoreProvider from './store/store-provider';
 import { DataFetcher } from './component/data-fetcher';
 import { VeilederverktoyModalController } from './component/veilederverktoy/veilederverktoy-components/veilederverktoy-modal-controller';
 import './index.less';
@@ -32,23 +31,21 @@ function App({ fnr, enhet, tilbakeTilFlate, visVeilederVerktoy, skjulEtiketter }
                 tilbakeTilFlate
             }}
         >
-            <StoreProvider>
-                <div className="visittkortfs">
-                    <DataFetcher>
-                        {brukerFnr => (
-                            <>
-                                <Tilbakelenke />
-                                <div className="visittkortfs__container">
-                                    <PersonInfo brukerFnr={brukerFnr} />
-                                    {!skjulEtiketter && <Etiketter brukerFnr={brukerFnr} />}
-                                    <Veilederverktoy />
-                                </div>
-                            </>
-                        )}
-                    </DataFetcher>
-                    <VeilederverktoyModalController />
-                </div>
-            </StoreProvider>
+            <div className="visittkortfs">
+                <DataFetcher>
+                    {brukerFnr => (
+                        <>
+                            <Tilbakelenke />
+                            <div className="visittkortfs__container">
+                                <PersonInfo brukerFnr={brukerFnr} />
+                                {!skjulEtiketter && <Etiketter brukerFnr={brukerFnr} />}
+                                <Veilederverktoy />
+                            </div>
+                        </>
+                    )}
+                </DataFetcher>
+                <VeilederverktoyModalController />
+            </div>
         </VisittKortConfigContext.Provider>
     );
 }
