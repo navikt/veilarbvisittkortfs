ef4f1c2b28bfead967e8eb0dfae8f9b8e07e0e2e
sigurdgroneng <sigurd.groneng@nav.no>
2026-01-28 08:12:07 +0100

    Add missing stlyes

--- src/webComponentWrapper.tsx
+++ src/webComponentWrapper.tsx
@@ -4,6 +4,25 @@ import App from './app';
 import './index.less?inline';
 import visittkortLessStyles from './index.less?inline';
 import visittkortCssStyles from './index.css?inline';
+import sijStyles from './component/components/sokfilter/sok-filter.less';
+import kopierStyles from './component/components/kopier-knapp/kopier-knapp.less';
+import radioStyles from './component/components/radiofilterform/radio-filterform.less';
+import varselModalStyles from './component/components/varselmodal/varsel-modal.less';
+import tilbakelenkeStyles from './component/components/tilbakelenke/tilbakelenke.less';
+import lasterModalStyles from './component/components/lastermodal/laster-modal.less';
+import formikStyles from './component/components/formik/formik.less';
+import huskelappPostitstylingStyles from './component/huskelapp/huskelapp-postitstyling.less';
+import huskelappRedigeringStyles from './component/huskelapp/redigering/huskelapp-redigering.less';
+import dataFetcherStyles from './component/data-fetcher.less';
+import personinfoStyles from './component/personinfo/personinfo.less';
+import etiketterStyles from './component/personinfo/components/etiketter.less';
+import tildelVeilederStyles from './component/veilederverktoy/tildel-veileder/tildel-veileder.less';
+import begrunnelseFormStyles from './component/veilederverktoy/begrunnelseform/begrunnelse-form.less';
+import historikkStyles from './component/veilederverktoy/historikk/historikk.less';
+import stoppEskaleringStyles from './component/veilederverktoy/stopp-eskalering/stopp-eskalering.less';
+import veilederverktoyStyles from './component/veilederverktoy/veilederverktoy.less';
+import selectMedSokStyles from './component/veilederverktoy/opprett-oppgave/components/select-med-sok/select-med-sok.less';
+import opprettOppgaveStyles from './component/veilederverktoy/opprett-oppgave/opprett-oppgave.less';
 
 class VisittkortElement extends HTMLElement {
     private root: ReactDOM.Root | null = null;
@@ -54,7 +73,29 @@ class VisittkortElement extends HTMLElement {
                         shadowrootmode: 'closed'
                     }}
                 >
-                    <style>{visittkortCssStyles + visittkortLessStyles}</style>
+                    <style>
+                        {visittkortCssStyles +
+                            visittkortLessStyles +
+                            sijStyles +
+                            kopierStyles +
+                            radioStyles +
+                            varselModalStyles +
+                            tilbakelenkeStyles +
+                            lasterModalStyles +
+                            formikStyles +
+                            huskelappPostitstylingStyles +
+                            huskelappRedigeringStyles +
+                            dataFetcherStyles +
+                            personinfoStyles +
+                            etiketterStyles +
+                            tildelVeilederStyles +
+                            begrunnelseFormStyles +
+                            historikkStyles +
+                            stoppEskaleringStyles +
+                            veilederverktoyStyles +
+                            selectMedSokStyles +
+                            opprettOppgaveStyles}
+                    </style>
                     <App
                         fnr={fnr}
                         enhet={enhet}
