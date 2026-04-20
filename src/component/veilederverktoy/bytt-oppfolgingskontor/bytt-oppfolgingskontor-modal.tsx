6c1fb6a0689a4904821326b2ef6135d6676ed420
Mathias S. Müller <mathias.sundt.muller@nav.no>
2026-04-20 16:35:17 +0200

    Fjern eslint-disable som eslint sa var unødvendig

--- src/component/veilederverktoy/bytt-oppfolgingskontor/bytt-oppfolgingskontor-modal.tsx
+++ src/component/veilederverktoy/bytt-oppfolgingskontor/bytt-oppfolgingskontor-modal.tsx
@@ -23,7 +23,6 @@ function ByttOppfolgingskontorModal({ brukerFnr }: { brukerFnr: string }) {
         data: alleKontorData,
         error: hentAlleKontorError,
         isLoading: hentAlleKontorLoading
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } = useSWR(brukerFnr ? `/kontorer/${brukerFnr}` : null, () => hentAlleKontor(brukerFnr));
 
     const {
