04760edc1e3d298ac8f62db3a488c492142e9f2b
Mads Lee Giil <74543378+madsleegiil@users.noreply.github.com>
2026-06-23 10:21:08 +0200

    Fjern toggle bruk-ao-kontor som ikke lenger er nødvendig (#975)

--- src/api/veilarbaktivitet.ts
+++ src/api/veilarbaktivitet.ts
@@ -1,19 +1,6 @@
-import { axiosInstance, get } from './utils';
+import { axiosInstance } from './utils';
 import { AxiosPromise } from 'axios';
-import useSWR from 'swr';
 
 export function fetchHarArenaTiltak(fnr: string): AxiosPromise<boolean> {
     return axiosInstance.get<boolean>(`/veilarbaktivitet/api/arena/harTiltak?fnr=${fnr}`);
 }
-
-const BRUK_AO_KONTOR_SOM_MASTER = 'bruk_ao_kontor_som_master';
-interface DabUnleashFeatures {
-    [BRUK_AO_KONTOR_SOM_MASTER]: boolean;
-}
-const togglesQueryParam = [BRUK_AO_KONTOR_SOM_MASTER].map(element => 'feature=' + element).join('&');
-const dabUnleashTogglesUrl = `/veilarbaktivitet/api/feature?${togglesQueryParam}`;
-
-export const useBrukAoKontorSomMaster = () => {
-    const { data } = useSWR<DabUnleashFeatures>(dabUnleashTogglesUrl, () => get(dabUnleashTogglesUrl));
-    return data ? data[BRUK_AO_KONTOR_SOM_MASTER] || false : false;
-};
