838d61c052b2f47f8b486061dd41f41e409e36d8
Kjell-Magne.Larsen <kjell-magne.larsen@nav.no>
2026-09-13 23:49:22 +0200

    Custom warning ikon for å overstyre default.

--- src/component/components/utgatt-forlengelse-alert/utgattForlengelseAlert.tsx
+++ src/component/components/utgatt-forlengelse-alert/utgattForlengelseAlert.tsx
@@ -5,6 +5,7 @@ import { useOppfolging } from '../../../api/veilarboppfolging';
 import { erUtmeldingsKandidat } from '../../veilederverktoy/forleng-oppfolging/utils';
 import { LeaveIcon, TimerStartIcon } from '@navikt/aksel-icons';
 import { useModalStore } from '../../../store/modal-store';
+// import { ExclamationmarkTriangleFillIcon } from '@navikt/aksel-icons';
 
 export const UtgattForlengelseAlert = () => {
     const [skjulModal, setSkjulModal] = useState<boolean>(false);
@@ -25,8 +26,14 @@ export const UtgattForlengelseAlert = () => {
     if (skjulModal) return null;
 
     return (
-        <GlobalAlert status="warning" centered={false} size="small" className="w-full outline-ax-bg-warning-moderate">
+        <GlobalAlert
+            status="warning"
+            centered={false}
+            size="small"
+            className="w-full outline-ax-bg-warning-moderate **:[[class*='alert']]:before:hidden"
+        >
             <GlobalAlert.Header className="items-center w-full bg-ax-bg-warning-moderate p-2">
+                {/*<ExclamationmarkTriangleFillIcon aria-hidden className="text-ax-text-warning text-red-600" /> */}
                 <GlobalAlert.Title className="text-ax font-ax-regular text-ax-text-warning">
                     Skal denne brukeren fortsatt ha oppfølging?
                 </GlobalAlert.Title>
